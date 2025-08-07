5dffa9c364d9a4524913a3330778a11e957f685d
Debottam Kar <debottamkar2003@gmail.com>
2025-08-07 14:31:33 +0530

    fixes

--- ai.js
+++ ai.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 const prompt = "welcome";
 
